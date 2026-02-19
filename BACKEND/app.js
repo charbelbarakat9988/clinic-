d9a5d9dc276acebdf6b6e7190f360084806169de
Charbel Barakat <charbelbarakat2018@gmail.com>
2026-02-19 11:52:22 -0800

    switch bcrypt to bcryptjs

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -41,3 +41,10 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use(cors({
+  origin: [
+    "https://vetclinic1.netlify.app"
+  ],
+  credentials: true
+}));
--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router(); // ✅ MUST BE DEFINED
 const db = require("../db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // REGISTER
